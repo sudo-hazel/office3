6a192926f59f5f93202af98ce572a74b50fc0449
Glitch (completecarev0-1) <none>
2021-07-12 12:29:36 +0000

    🚃🚲 Updated with Glitch

--- public/wsout.js
+++ public/wsout.js
@@ -19,18 +19,6 @@ window.WS.ws.onmessage = (ev) => {
         .getElementsByClassName(data[1].classes)[0].value = data[1].value;
     }
   }
-  if (type === "update") {
-    let data = msg.data;
-
-    if (data[0] === "new-pt") {
-      pNameToElm(data[1].name, data[1].id);
-    }
-    if (data[0] === "type-sync") {
-      document
-        .getElementById(data[1].id)
-        .getElementsByClassName(data[1].classes)[0].value = data[1].value;
-    }
-  }
 };
 window.WS.ws.onclose = function (event) {
   //this doesn't really work lol
